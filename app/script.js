3dbb5940928c6828ba706376cc4d52cf7c720657
Adam Goss <adam.goss94@gmail.com>
2020-06-08 18:41:04 +0200

    Add state, startTimer and formatTime functions

--- app/script.js
+++ app/script.js
@@ -2,24 +2,61 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  state = {
+    status: 'off',
+    time: 0,
+    timer: null,
+  }
+
+  formatTime = time => {
+
+    let minutes = Math.floor(time / 60);
+    let seconds = time - (minutes * 60);
+
+    if(minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if(seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    return minutes + ' : ' + seconds;    
+  };
+
+  step = () => {
+    
+  };
+
+  startTimer = () => {  
+    this.setState({
+      status: 'work',
+      time: 1200,
+      timer: setInterval(this.step, 1000),
+    });
+  
+  };
 
   render() {
+    const { status } = this.state;
+
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
+        {(status === 'off') &&
+          <div>
+            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+            <p>This app will help you track your time and inform you when it's time to rest.</p>
+          </div>
+        }
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">{this.formatTime(this.state.time)}</div>}
+        {(status === 'off') && <button className="btn" onClick={this.startTimer}>Start</button>}
+        {(status !== 'off') && <button className="btn">Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
     )
-  }
+  };
 };
 
 render(<App />, document.querySelector('#app'));
